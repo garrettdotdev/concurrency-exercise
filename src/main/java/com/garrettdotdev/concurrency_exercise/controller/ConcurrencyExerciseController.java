5c7ee54affb371f759aff725ba9ecadba8ab140b
Garrett Rohde <garrettrohde86@gmail.com>
2025-02-06 16:41:50 -0500

    Change controller methods to utilize service class.

--- src/main/java/com/garrettdotdev/concurrency_exercise/controller/ConcurrencyExerciseController.java
+++ src/main/java/com/garrettdotdev/concurrency_exercise/controller/ConcurrencyExerciseController.java
@@ -1,5 +1,7 @@
 package com.garrettdotdev.concurrency_exercise.controller;
 
+import com.garrettdotdev.concurrency_exercise.service.ConcurrencyExerciseService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,18 +10,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class ConcurrencyExerciseController {
 
+    private final ConcurrencyExerciseService concurrencyExerciseService;
+
+    public ConcurrencyExerciseController(ConcurrencyExerciseService concurrencyExerciseService) {
+        this.concurrencyExerciseService = concurrencyExerciseService;
+    }
+
     @GetMapping("one")
-    public String endpointOne() {
-        return "this is one";
+    public ResponseEntity<String> endpointOne() {
+        return concurrencyExerciseService.handleOne();
     }
 
     @GetMapping("two")
-    public String endpointTwo() {
-        return "this is two";
+    public ResponseEntity<String> endpointTwo() {
+        return concurrencyExerciseService.handleTwo();
     }
 
     @GetMapping("three")
-    public String endpointThree() {
-        return "this is three";
+    public ResponseEntity<String> endpointThree() {
+        return concurrencyExerciseService.handleThree();
     }
 }
