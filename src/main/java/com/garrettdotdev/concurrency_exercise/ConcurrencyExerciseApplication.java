075f683cf448f4cfedb4326e87ba0ae13d92c6a0
Garrett Rohde <garrettrohde86@gmail.com>
2025-02-06 15:24:07 -0500

    Initial commit

---
+++ src/main/java/com/garrettdotdev/concurrency_exercise/ConcurrencyExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.garrettdotdev.concurrency_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrencyExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/garrettdotdev/concurrency_exercise/ConcurrencyExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garrettdotdev.concurrency_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
